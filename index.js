3d8211815549f1851571409a13f34d8e5e73cf5e
harshu4 <harsh4444.hf@gmail.com>
2022-12-03 03:40:13 +0530

    changed names

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const call = require("./infura.js");
+const call = require("./helper/listenEvent.js");
 const app = express();
 var bodyParser = require('body-parser')
 const port = 4040;
@@ -32,7 +32,7 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 
 
-    call.handlecontracts({ "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889": ['Deposit', 'Withdrawal'] }, tell)
+    
 });
 
 
